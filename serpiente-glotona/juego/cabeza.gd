374da1e61c967526d2f3195d453511e7b95f55ed
Mixcoatl69 <ferruchis20003@gmail.com>
2025-11-04 13:38:23 -0600

    Jugador y preparación de juego
    el jugador se puede mover y ya esta lista la preparación para iniciar con el resto de mecánicas del juego.

---
+++ serpiente-glotona/juego/cabeza.gd
@@ -0,0 +1,5 @@
+class_name cabeza extends parte_de_serpiente
+
+
+
+	
--- serpiente-glotona/juego/juego.gd
+++ serpiente-glotona/juego/juego.gd
@@ -1,12 +1,18 @@
 class_name Juego extends Node
 
+@onready var Cabeza: cabeza = $Cabeza as cabeza
+
+
+var time_between_moves:float = 1000.0
+var time_since_last_move:float = 0 
+var speed:float = 1000.0
 var move_dir:Vector2 = Vector2.RIGHT
 
 func _ready() -> void:
 	pass
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	
 	if Input.is_action_just_pressed("ui_up"):
 		move_dir = Vector2.UP
@@ -16,3 +22,15 @@ func _process(delta: float) -> void:
 		move_dir = Vector2.LEFT
 	if Input.is_action_just_pressed("ui_right"):
 		move_dir = Vector2.RIGHT
+
+func _physics_process(delta: float) -> void:
+	time_since_last_move += delta * speed
+	if time_since_last_move >= time_between_moves:
+		update_serpiente()
+		time_since_last_move = 0 
+
+func update_serpiente():
+	print("mueve la serpiente")
+	var new_pos:Vector2 = Cabeza.position + move_dir * Global.GRID_SIZE
+	Cabeza.move_to(new_pos)
+	pass
